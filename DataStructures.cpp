d1fa90869d32f9c893c5df17452c32c962a1c26a
AqsaZeeshan <aqsa2023123@gmail.com>
2024-12-07 11:14:35 +0500

    Created a class for dynamic array
    created a class to handle dynamic arrays to replace vectors. Will have to modify the sorting algos to work with dynamic array class instead of vectors now. will do that later

---
+++ DataStructures.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include<Menu.cpp>
+using namespace std;
+
+class DynamicArray
+{
+    private:
+        File* files;
+        int capacity;
+        int size;
+
+        void resize()
+        {
+            capacity = capacity * 2;
+            File* newFiles = new File[capacity];
+            for(int i = 0; i < size; i++)
+            {
+                newFiles[i] = files[i];
+            }
+            delete[] files;
+            files = newFiles;
+        }
+    public:
+        DynamicArray(int initialCapacity = 10)
+        {
+            capacity = initialCapacity;
+            size = 0;
+            files = new File[capacity];
+        }
+
+        void addFile(const File& file)
+        {
+            if(size == capacity)
+            {
+                resize();
+            }
+            files[size++] = file;
+        }
+
+        int getSize()
+        {
+            return size;
+        }
+
+        File& operator[](int index)
+        {
+            if(index < 0 || index >= size)
+            {
+                throw out_of_range("Index out of range");
+            }
+            return files[index];
+        }
+
+};
+
+
+
+
+
+int main()
+{
+
+}
