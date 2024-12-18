97efd390b29048e13f7f587e76f66606d0b2f49e
muhammad-abdullah-farrukh <mabdullahfarrukh05@gmail.com>
2024-12-18 19:45:17 +0500

    removed comments

--- newtest.cpp
+++ newtest.cpp
@@ -1,6 +1,4 @@
 //created this to edit fucntions and test codes, while keeping the initial code in test.cpp
-// shitty farrukh
-// meow aqsa is the greatest
 #include <iostream>
 #include <vector>
 #include <string>
